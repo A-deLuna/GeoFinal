f1dbf5b4d450a96cf7d0fe1834c687879e9e353b
OneKorg <tonydelun@gmail.com>
2015-05-21 07:21:21 -0700

    initial ssdf

---
+++ ProyectoFinal/PuntosWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProyectoFinal
+{
+	
+	public partial class PuntosWindow : Gtk.Window
+	{
+		private Gtk.NodeStore store;
+		public PuntosWindow () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+
+		protected void OnNodeViewRealized (object sender, EventArgs e)
+		{
+			
+		}
+
+		protected void onNodeViewExposed (object o, Gtk.ExposeEventArgs args)
+		{
+			Gtk.NodeView nodeView = (Gtk.NodeView)o;
+			nodeView.AppendColumn ("Nombre", new Gtk.CellRendererText (), "text", 0);
+			nodeView.AppendColumn ("X", new Gtk.CellRendererText (), "text", 1);
+			nodeView.AppendColumn ("Y", new Gtk.CellRendererText (), "text", 2);
+			nodeView.NodeStore.AddNode(
+			nodeView.ShowAll ();
+		}
+	}
+}
+
